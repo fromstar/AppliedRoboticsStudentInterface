a6807efd1fe23aac9974155e3c19e765f9befa6a
Davide-1998 <bertellidavide98@gmail.com>
2022-06-01 11:15:25 +0200

    Changed the logger class: it prints out the seconds elapsed among events

--- src/Log/logger.cpp
+++ src/Log/logger.cpp
@@ -1,35 +1,141 @@
 #include "logger.h"
 
-void logger::set_log_path(string path){
-	default_log_file_path = path;
+/**
+ * \fun
+ * This is the default constructor for the float_node struct.
+ */
+float_node::float_node(double v){
+	value = v;
 };
 
+/**
+ * \fun
+ * This method is used to add a node to the list of double values.
+ * @param v: double. It is the double value to add to the list.
+ */
+void float_list::add_node(double v){
+	if( head == NULL){
+		head = new float_node(v);
+		tail = head;
+	}else{
+		tail->next = new float_node(v);
+		tail = tail->next;
+	};
+	size++;
+};
+
+/**
+ * \fun
+ * This is the custom destructor for the float_list struct.
+ * It is used to deallocate all the memory used by the float values.
+ */
+float_list::~float_list(){
+	if (head != NULL){
+		float_node* tmp = head;
+		while(tmp->next != NULL){
+			float_node* prev = tmp;
+			tmp = tmp->next;
+			delete(prev);
+		};
+		head = NULL;
+		tail = NULL;
+		size = 0;
+	};
+};
+
+/**
+ * \fun
+ * This method is the default constructor for the logger class.
+ * @param path_to_logger: string. It is the path in which the logging file
+ * will be saved.
+ */
+logger::logger(string path_to_logger){
+	set_log_path(path_to_logger);
+};
+
+/**
+ * \fun
+ * This method customizes the destructor of the logger class.
+ */
+logger::~logger(){
+	events->~float_list();;
+};
+
+/**
+ * \fun
+ * This method is used to set the location in which the logger file will be
+ * written/updated.
+ * @param path: string. It is the path to the file.
+ */
+void logger::set_log_path(string path){
+	default_log_file_path = path;
+	
+	// Initialize the file in the new path
+	FILE* log = fopen(get_log_path(), "a");
+	fprintf(log, "DATE        - TIME     - ELAPSED TIME\n");
+	fclose(log);
+};
+
+/**
+ * \fun
+ * This method is used to return an array of chars containing the location
+ * of the logging file.
+ */
 const char* logger::get_log_path(){
 	return default_log_file_path.c_str();
 };
 
+/**
+ * \fun
+ * This method is used to add a new event to the logging file.
+ * @param event: string. It is the message describing the event to be added.
+ */
 void logger::add_event(string event){
 	FILE* log = fopen(get_log_path(), "a");
 
 	time_t timer = time(0);
 	tm* event_time = localtime(&timer);
+	
+	float t = float(clock())/CLOCKS_PER_SEC;
+	double clocks = 0;
+	if ( events == NULL ){
+		events = new float_list();
+	}else{
+		clocks = t - events->tail->value;
+	};
+	events->add_node(t);
 
-	fprintf(log, "[%02d/%02d/%04d - %02d:%02d:%02d]: %s\n",
+	fprintf(log, "[%02d/%02d/%04d - %02d:%02d:%02d - %0.6f]: %s\n",
 		   event_time->tm_mday, event_time->tm_mon, 1900 + event_time->tm_year,
 		   event_time->tm_hour, event_time->tm_min, event_time->tm_sec,
+		   clocks,
 		   event.c_str());
 
 	fclose(log);
-
-	n_events += 1;
 };
 
+/**
+ * \fun
+ * This method is used to wipe out the data contained in the logging file.
+ * The file itself will not be deleted from the disk.
+ */
 void logger::clear(){
 	FILE* log = fopen(get_log_path(), "w");
 	fclose(log);
-	n_events = 0;
+	events->~float_list();
+	events->size = 0;
 };
 
+/**
+ * \fun
+ * This method is used to print the content of the logging file.
+ * The method allows also for an implicit update of the logger structure
+ * from a saved file. If the flag \"update\" will be set to true all the
+ * lines read from the file will be added as events inside the logger
+ * structure.
+ * @param update: bool. It is the flag telling whether or not to add
+ * the read events to the logger structure.
+ */
 void logger::history(bool update){
 	FILE* log = fopen(get_log_path(), "r");
 	if (log == NULL){
@@ -48,12 +154,18 @@ void logger::history(bool update){
 	fclose(log);
 
 	if(update){
-		n_events = recorded_events; 
+		events->size = recorded_events; 
 	};
 
 };
 
+
+/**
+ * \fun
+ * This method prints the path of the logging file and the number of events
+ * recorded.
+ */
 void logger::info(){
 	cout << "Default path is: " << default_log_file_path << endl <<
-			"Events recorded: " << n_events << endl;
+			"Events recorded: " << events->size << endl;
 };
--- src/Log/logger.h
+++ src/Log/logger.h
@@ -7,6 +7,38 @@
 #include <iostream>
 
 using namespace std;
+
+/**
+ * This structure is used as a storage for the clock time used by the program.
+ * @param value: double. It is the value of type double contained
+ * @param next: float_node\*. It is the pointer to the next double value.
+ */
+typedef struct float_node{
+	double value = 0.0;
+	float_node* next = NULL;
+
+	float_node(double v);
+}float_node;
+
+/**
+ * This struct is the list used to store the clock values.
+ * @param head: float_node\*. It is the pointer to the head of the list;
+ * @param tail: float_node\*. It is the pointer to the tail of the list;
+ * @param size: int. It is the current size of the list.
+ * 
+ * Methods available are:
+ * @see add_node(): It allows to add a new node to the list.
+ * @see ~float_lost(): It is the custom destructor of the list.
+ */
+typedef struct float_list{
+	float_node* head = NULL;
+	float_node* tail = NULL;
+	int size = 0;
+
+	void add_node(double v);
+	~float_list();
+}float_list;
+
 /**
  * This class is an abstraction of a log file. It allows to write what the
  * program does to a file with the knowledge of time and date in which events
@@ -16,6 +48,7 @@ using namespace std;
  * is going to be saved and updated.
  * @param n_events: int. Is the number of events that have been recorded.
  * The methods available are:
+ * @see logger(): It is the zero parameters constructor of the logger class.
  * @see get_log_path(): returns the path in which the log.txt is saved.
  * @see set_log_path(string path): change the path in which the log.txt is.
  * @see add_event(string event): adds a new record to the log.
@@ -23,18 +56,21 @@ using namespace std;
  * @see history(bool update): prints all the records in the log file. If update
  * is set to true it will update the n_events private parameter.
  * @see clear(): is the function responsible to wipe out the log file.
+ * @see ~logger(): It is the custom destructor of the logger class.
  */
 typedef class logger{
 	private:
-		string default_log_file_path = "log.txt";
-		int n_events = 0;
+		string default_log_file_path;
+		float_list *events = NULL;
 	public:
+		logger(string path_to_logger="log.txt");
 		const char* get_log_path();
 		void set_log_path(string path);
 		void add_event(string event);
 		void info();
 		void history(bool update=false);
 		void clear();
+		~logger();
 }logger;
 
 #endif
