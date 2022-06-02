8312d258cab31404554d232b9d5a033600a8bec7
Davide-1998 <bertellidavide98@gmail.com>
2022-06-02 10:22:43 +0200

    Added documentation

--- src/Robot_manager/robot_manager.cpp
+++ src/Robot_manager/robot_manager.cpp
@@ -1,9 +1,23 @@
 #include "robot_manager.h"
 
+/**
+ * \fun
+ * This is the default constructor of the robot_manager struct.
+ * @param l: logger\*. It is the logger instance which will be used to save
+ * the events.
+ */
 robot_manager::robot_manager(logger* l){
 	rm_logger = l;
 };
  
+/**
+ * \fun
+ * This method allows to add a robot to the struct.
+ * @param r: Robot\*. It is a pointer to the Robot instance to be added to
+ * the manager.
+ * @param f_path: string. It is the path in which the robot will save files
+ * necessary to perform its tasks.
+ */
 void robot_manager::add_robot(Robot* r, string f_path){
 	switch(r->type){
 		case catcher:
@@ -13,23 +27,37 @@ void robot_manager::add_robot(Robot* r, string f_path){
 			fugitives[r->ID] = new robot_fugitive(r, f_path);
 			break;
 		default:
-			// All robots not fugitives are treated as potential thrests
-			// hence catchers
+			// All robots non fugitives are treated as potential threats for
+			// the others hence catchers.
 			catchers[r->ID] = new robot_catcher(r, f_path);
 			break;
 	};
 	rm_logger -> add_event("Added robot " + r->ID + " to the robot manager");
 };
 
+/**
+ * \fun
+ * This method allows to parse a map of Robot instance and add its content
+ * to the menager.
+ * @param map_r: map<string, Robot\*>. It is the map containing the Robot
+ * instances using as keys the ID of the robots.
+ * @param f_path: string. It is the path in which the added robots will save
+ * the files necessary for their execution.
+ */
 void robot_manager::parse_map_robots(map<string, Robot*> map_r, string f_path){
 	for( map<string, Robot*>::iterator it = map_r.begin(); it != map_r.end();
 		 ++it){
 		add_robot(it->second, f_path);
 	};
-	
-	rm_logger -> add_event("After parsing added " + map_r.size() + string(" robots"));
+	rm_logger -> add_event("After parsing added " + map_r.size() +
+						   string(" robots"));
 };
 
+/**
+ * \fun
+ * This method is used to chose the antagonist of each robot.
+ * It works under geometrical assumptions.
+ */
 void robot_manager::trade_fugitives(){
 	if (fugitives.size() == catchers.size()){
 		map<string, robot_fugitive*>::iterator it_f = fugitives.begin();
@@ -44,6 +72,14 @@ void robot_manager::trade_fugitives(){
 	rm_logger -> add_event("Ended Robot trading");
 };
 
+/**
+ * \fun
+ * This method is used to print relevant informations regarding the robot
+ * manager.
+ * @param detailed: bool. It is a flag telling whether or not to show also
+ * the informations regarding the robots contained in the robot manager.
+ * It is setted to false by default.
+ */
 void robot_manager::info(bool detailed){
 	cout << "Robot Menager info:" << endl
 		 << "\tCatchers available: " << catchers.size() << endl
